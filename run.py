61a6255e35f523afc84f0a142214e56d289be4be
Douglas <douglas.ofc.2004@gmail.com>
2024-12-21 12:46:52 -0300

    Configuração inicial do servidor Flask

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-print("O Docker está funcionando corretamente!")
-while True:
-    pass
+from app import create_app
+
+app = create_app()
+
+@app.route("/")
+def home():
+    return "Flask está funcionando!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
