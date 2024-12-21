644fc059c033692277ee66c70ca9917fb6ce8a5a
Douglas <douglas.ofc.2004@gmail.com>
2024-12-21 18:48:31 -0300

    Cria o Endpoint de Status do Banco de dados

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -8,11 +8,27 @@ def test_db():
     try:
         database_connection = get_db_connection()
         cursor_execucao = database_connection.cursor()
-        cursor_execucao.execute("SELECT 1")
-        result = cursor_execucao.fetchone()
+        
+        cursor_execucao.execute("SELECT setting FROM pg_settings WHERE name = 'max_connections';")
+        max_connections = cursor_execucao.fetchone()[0]
+
+        cursor_execucao.execute("SELECT COUNT(*) FROM pg_stat_activity WHERE datname = current_database();")
+        active_connections = cursor_execucao.fetchone()[0]
+        
+        cursor_execucao.execute("SELECT NOW();")
+        date = cursor_execucao.fetchone()[0]
+        
+        cursor_execucao.execute("SELECT version();")
+        db_version = cursor_execucao.fetchone()[0]
+        
+        
         database_connection.close()
         
-        return jsonify({"status": "success", "result": result}), 200
+        return jsonify({
+            "Active_connections": active_connections,
+            "Current Date": str(date), 
+            "DB Version": db_version,
+            "Max Connections": int(max_connections)}), 200
     
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
