67b7c1ee2fdf627fe9425efb1ebe45a4b9c062a5
Douglas <douglas.ofc.2004@gmail.com>
2024-12-28 15:41:17 -0300

    Adiciona testes para o endpoint de usuário, incluindo casos de sucesso e erro de conexão

--- tests/test_user_controller.py
+++ tests/test_user_controller.py
@@ -1,6 +1,6 @@
 import pytest
 from app import create_app
-from datetime import datetime
+from unittest.mock import patch
 
 @pytest.fixture
 def client():
@@ -32,4 +32,32 @@ def test_status_database(client):
     assert "Max Connections" in responseBody
     assert responseBody["Max Connections"] == 100
 
+def test_get_user_existente(client):
 
+    response= client.get('api/user/1')
+    responseBody= response.get_json()
+    assert response.status_code == 200
+
+    assert responseBody["id"] == 1
+    assert responseBody["name"] == "User1"
+    assert responseBody["email"] == "user1@example.com"
+
+
+def test_get_user_naoencontrado(client):
+
+    response= client.get('api/user/9999')
+    responseBody= response.get_json()
+    assert response.status_code == 404
+    assert "message" in responseBody
+    assert responseBody["message"] == "User not found"
+
+def test_get_user_errodeconexao(client):
+
+    with patch("app.config.get_db_connection", side_effect=Exception("Database connection error")):
+        response= client.get('api/user/1')
+        responseBody= response.get_json()
+        assert response.status_code == 500
+    
+        assert "message" in responseBody
+        assert responseBody["message"] == "Internal server error"
+    
